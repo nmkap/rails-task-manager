a18e2127bf2c2b3f5963d3967b4d7aaa34a7e45d
Wiktoria Sudak wiktoria.sudak@icloud.com <wiktoria.sudak@icloud.com>
2025-11-18 14:04:03 +0000

    Bug with new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,35 @@
 class TasksController < ApplicationController
+  # As a user, I can list tasks (display all)
+  # get "tasks", to: "tasks#index"
+  # As a user, I can view the details of a task (find by id)
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # As a user, I can add a new task (create)
+  # get "tasks/new", to: "tasks#new",  as: :new_task
+  # post "tasks", to: "tasks#create"
+  # As a user, I can edit a task (mark as completed / update title & details) (update)
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+  # As a user, I can remove a task (destroy)
+  # delete "tasks/:id", to: "tasks#destroy"
+
+  def index
+    # First, add a new route to list the tasks, following the convention from the lecture.
+    # Then, add a controller action and its view.
+    # This action should fetch all tasks,
+    # and a view should loop over these to display them,like in the screenshot below.
+    @tasks = Task.all
+    # Now we need to create a corresponding view page,
+    # for the user to be able to see all the listed tasks.
+  end
+
+  def show
+    @task = Task.find(params[:id])
+
+  end
+
+  def create
+    @task = Task.new
+    @task.save
+    redirect_to task_path(@task)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # As a user, I can list tasks (display all)
+  get "tasks", to: "tasks#index"
+  # As a user, I can view the details of a task (find by id)
+  get "tasks/:id", to: "tasks#show", as: :task
+  # # As a user, I can add a new task (create)
+  get "tasks/new", to: "tasks#new", as: :new_task
+  # post "tasks", to: "tasks#create"
+  # # As a user, I can edit a task (mark as completed / update title & details) (update)
+  #  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+  # # As a user, I can remove a task (destroy)
+  #  delete "tasks/:id", to: "tasks#destroy"
 end
